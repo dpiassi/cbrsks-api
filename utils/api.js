31dbf4e8989cf3d394d9373077a4e683aa3e99b3
Marco Bruno <marco.bruno@meliuz.com.br>
2023-01-21 12:17:03 +0100

    :sparkles: Add the function key to GetUserByDiscordId

--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,11 @@
 const { request } = require('undici')
 
-const {API_URL} = process.env
+const {API_URL, KEY_GET_USER_BY_DISCORD_ID} = process.env
 
 const api = {
   getUserByDiscordId: async (id =  false) => {
     try {
-      const userDBResponse = await request(`${API_URL}/users/discord/${id}`)
+      const userDBResponse = await request(`${API_URL}/users/discord/${id}?code=${KEY_GET_USER_BY_DISCORD_ID}`)
       const userDB = await userDBResponse.body.json()
   
       return userDB
