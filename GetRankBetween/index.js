3a824e6378c3c76319f95d3f7cc5b9881aa2ea6d
Marco Bruno <marco.bruno@meliuz.com.br>
2023-01-30 01:02:35 +0100

    :construction: Mock /rank/start/end

---
+++ GetRankBetween/index.js
@@ -0,0 +1,92 @@
+module.exports = async (context, req) => {
+  const rank = [
+    {
+      rank: 1,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "01:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 2,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "02:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 3,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "03:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 4,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "04:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 5,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "05:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 6,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "06:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 7,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "07:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 8,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "08:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 9,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "09:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    },
+    {
+      rank: 10,
+      username: "demiannpi",
+      discriminator: "6476",
+      avatar: "https://cdn.discordapp.com/avatars/685887129422790667/a9cd8d86d76cb0eb4360a994fea22b29",
+      time: "10:00.000",
+      corporation: "https://cbrsks-unity.s3.amazonaws.com/images/corporations/OH-White.png"
+    }
+  ]
+
+  return {
+    status: 200,
+    body: {
+      status: 200,
+      rank
+    }
+  }
+}
