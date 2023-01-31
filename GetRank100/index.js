9477f2224f96e95e0fd3e1207fca4d05a900cc10
Marco Bruno <marco.bruno@meliuz.com.br>
2023-01-31 16:01:25 +0100

    :sparkles: Remove duplicates in ranking

--- GetRank100/index.js
+++ GetRank100/index.js
@@ -1,12 +1,26 @@
 module.exports = async (context, req) => {
   try {
-    const ranking = context.bindings.inputSoloMatchesRanking
+    const ranking = context.bindings.inputSoloMatchesRanking.map((pos, index) => ({
+      ...pos,
+      rank: index + 1
+    }))
+    const result = []
+    const seen = new Set()
+
+    for (const item of ranking) {
+      const key = item.username + item.discriminator
+
+      if (!seen.has(key)) {
+        result.push(item)
+        seen.add(key)
+      }
+    }
   
     return {
       status: 200,
       body: {
         status: 200,
-        ranking
+        ranking: result.slice(0, 100)
       }
     }
   } catch(error) {
