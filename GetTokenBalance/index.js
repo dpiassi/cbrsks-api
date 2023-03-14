876a2e5714e7385ebd6270433c5a7438c88e6355
lucas <lucasfonsecard@icloud.com>
2023-03-14 04:23:04 -0300

    feat: add getTokenBalance endpoint

---
+++ GetTokenBalance/index.js
@@ -0,0 +1,24 @@
+const { getAssociatedTokenAddressSync } = require("@solana/spl-token");
+const { Connection, PublicKey } = require("@solana/web3.js");
+
+module.exports = async (context, req) => {
+  try {
+    const wallet = req.query.wallet || (req.body && req.body.wallet);
+    const walletPk = new PublicKey(wallet);
+    const mint = new PublicKey(process.env.MINT);
+    const connection = new Connection(process.env.RPC_URL, "confirmed");
+    const tokenAta = getAssociatedTokenAddressSync(mint, walletPk);
+    const tokenBalance = await connection.getTokenAccountBalance(tokenAta);
+
+    context.res = {
+      body: {
+        balance: tokenBalance.value.uiAmount,
+      },
+    };
+  } catch (error) {
+    console.log("GetTokenBalance", "ERROR", error);
+    context.res = {
+      status: 500
+    };
+  }
+};
